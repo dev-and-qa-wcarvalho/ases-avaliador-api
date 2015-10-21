b152e20cd5f587d3b0a41ab709860a988bf6b5dc
lyandro.santana <lyandro.santana@c2178572-b5ca-4887-91d2-9e3a90c7d55b>
2015-10-21 21:25:44 +0000

    Apresentação
    git-svn-id: https://svn.bento.ifrs.edu.br/default/ASES/ASES%20-%20Web/ASES%20-%20Web/Fontes/avaliador-api@10492 c2178572-b5ca-4887-91d2-9e3a90c7d55b

--- src/main/java/br/com/checker/emag/core/PresentationEvaluation.java
+++ src/main/java/br/com/checker/emag/core/PresentationEvaluation.java
@@ -3,10 +3,9 @@ package br.com.checker.emag.core;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
-import fj.data.hlist.HPre.HBool;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.htmlparser.jericho.Attribute;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Source;
@@ -72,22 +71,135 @@ public class PresentationEvaluation extends Evaluation{
 		AvaliadorContraste avaliadorContraste = new AvaliadorContraste();
 		Color color = null;
 		
-		boolean temBgcolor = false;
+		int foreground = Integer.parseInt("000000", 16);
+		int background = Integer.parseInt("ffffff", 16);
+		
+		//verificar css in-line
+		for (Element element : getDocument().getAllElements()) {
+			
+			if(element != null){
+				String style = element.getAttributeValue("style");
+				
+				if(style != null){
+					if(isContrastInferior(style)){
+						occurrences.add(buildOccurrence("4.1", true, element.toString(), element,"1"));
+					}			
+				}
+			}
+		}
+		
+		//verificar css in-line
+		boolean isValid = false;
+		for (Element element : getDocument().getAllElements("body")) {
+			if(element != null){
+				String style = element.getAttributeValue("style");
+				
+				if(style != null){
+					if(isContrastInferior(style)){
+						occurrences.add(buildOccurrence("4.1", true, element.toString(), element,"1"));
+						isValid = true;
+					}			
+				}
+		
+				if(!isValid)
+				for (Element body : getDocument().getAllElements("body")) {
+					Attribute bgc = body.getAttributes().get("bgcolor");
+					if(bgc != null){
+						
+						background = Integer.parseInt(bgc.getValue().trim().replaceFirst("^#",""), 16);
+						
+						avaliadorContraste.setCores(new Color(background), new Color(foreground));
+						avaliadorContraste.avaliar();
+						
+						if(avaliadorContraste.getContraste() < new Double("4.5")){
+							occurrences.add(buildOccurrence("4.1", true, body.getStartTag().toString(), body,"1"));
+							isValid = true;
+						}	
+					}
+					
+					if(body.getName().equals("font")){
+						String cor = element.getAttributeValue("color");
+						Color foregroundV = null;
+						 if(!isTextCor(cor)){
+					        if(cor.contains("rgb")){
+				        		String  str = cor.replaceAll("[^-?0-9]+", " "); 
+				        		
+				        		foregroundV = new Color(
+				        				Integer.parseInt(str.trim().split(" ")[0]),
+				        				Integer.parseInt(str.trim().split(" ")[1]),
+				        				Integer.parseInt(str.trim().split(" ")[2]));
+				        		
+				        	}else foregroundV = new Color(Integer.parseInt(cor.trim().replaceFirst("^#",""), 16));
+					     
+					        background = Integer.parseInt(bgc.getValue().trim().replaceFirst("^#",""), 16);
+							
+							avaliadorContraste.setCores(new Color(background), foregroundV);
+							avaliadorContraste.avaliar();
+							
+							if(avaliadorContraste.getContraste() < new Double("4.5")){
+								occurrences.add(buildOccurrence("4.1", true, body.getStartTag().toString(), body,"1"));
+								isValid = true;
+							}	
+						 
+						 }
+					}
+				}
+			}
+		}
+		
+		//verificar css interno
+		for (Element css : getDocument().getAllElements("style")) {
+			
+			if(css != null){
+				if(isContrastInferior(css.getContent().toString())){
+					occurrences.add(buildOccurrence("4.1", true, css.toString(), css,"1"));
+					isValid = true;
+				}		
+			}	
+		
+		}
+	
+		//verificar css externo
+		String href = null;
+		boolean avalia = false;
+		for(Element link : getDocument().getAllElements("link")) {
+			href = link.getAttributeValue("href");
+			if(href.startsWith("www"))href= "http://"+href;
+			
+			avalia = getUrl()!=null || href.startsWith("http");
+			
+			if(href.contains(".css") && avalia) {
+				
+				if(!href.startsWith("http")) href = getUrl()+"/"+href;
+				
+				String content = WebAgent.from(href.replace(" ", "%20")).withGetRequest().execute().getContent();
+				if(isContrastInferior(content)){
+					occurrences.add(buildOccurrence("4.1", true, link.toString(), link,"1"));
+					isValid = true;
+				}
+			}
+		}
+		
+		
+		
+		
+		/*boolean temBgcolor = false;
 		for (Element bgcolor : getDocument().getAllElements("body")) {
 			Attribute bgc = bgcolor.getAttributes().get("bgcolor");
 			if(bgc != null){
 				
-				int rgb = Integer.parseInt(bgc.getValue().replaceFirst("^#",""), 16);
-				color = new Color(rgb);
-				avaliadorContraste.setCores(color, color);
+				background = Integer.parseInt(bgc.getValue().replaceFirst("^#",""), 16);
+				
+				avaliadorContraste.setCores(new Color(background), new Color(foreground));
 				avaliadorContraste.avaliar();
 				
 				if(avaliadorContraste.getContraste() < new Double("4.5"))
-					occurrences.add(buildOccurrence("4.1", true, bgcolor.toString(), bgcolor,"1"));
+					occurrences.add(buildOccurrence("4.1", true, bgcolor.toString(), bgcolor,"3"));
 				
 				temBgcolor = true;
 			}
-		}
+		}	
+		
 		for (Element bgcolor : getDocument().getAllElements("table")) {
 			Attribute bgc = bgcolor.getAttributes().get("bgcolor");
 			if(bgc != null){
@@ -132,12 +244,112 @@ public class PresentationEvaluation extends Evaluation{
 				
 				temBgcolor = true;
 			}
-		}	
+		}	*/
 		
 		/*if(!temBgcolor)
 			occurrences.add(new Occurrence("4.1", false, getDocument().getFirstElement().toString(),OccurrenceClassification.PRESENTATION_DESIGN));*/
 		Collections.sort(occurrences);
 		return occurrences;
+			
+	}
+	
+	private boolean isTextCor(String cor){
+		return cor.matches("[a-zA-Z\\s]+");  
+	}
+	
+	private boolean isContrastInferior(String style){
+		
+		AvaliadorContraste avaliadorContraste = new AvaliadorContraste();
+		
+		/*int foreground = Integer.parseInt("000000", 16);
+		int background = Integer.parseInt("ffffff", 16);*/
+		boolean isProces = true;
+		
+		Color foregroundColor = new Color(Integer.parseInt("000000", 16)); 
+		Color backgroundColor = new Color(Integer.parseInt("ffffff", 16)); 
+		
+		
+		//foreground
+	   Pattern colorValue = Pattern.compile("color:(.*?)\\;");
+	   
+	    Matcher match = colorValue.matcher(style);
+	   
+	    if(match.find()){
+	    	
+	    	String cor = match.group(1);
+	    	
+	        //System.out.println("foreground: "+cor);
+	        if(!isTextCor(match.group(1)) && !cor.contains("url")){
+	        
+	        	if(cor.contains("rgb")){
+	        		String  str = cor.replaceAll("[^-?0-9]+", " "); 
+	        		
+	        		foregroundColor = new Color(
+	        				Integer.parseInt(str.trim().split(" ")[0]),
+	        				Integer.parseInt(str.trim().split(" ")[1]),
+	        				Integer.parseInt(str.trim().split(" ")[2]));
+	        		
+	        	}else foregroundColor = new Color(Integer.parseInt(cor.trim().replaceFirst("^#",""), 16));
+	        	
+	        }else  	isProces = false;
+	    }
+	    
+	    //background
+	    Pattern backgroundValue = Pattern.compile("background:(.*?)\\;");
+		match = backgroundValue.matcher(style);
+	    if(match.find()){
+	    	String cor = match.group(1);
+	    
+	       //System.out.println("background: "+cor);
+	       if(!isTextCor(cor) && !cor.contains("url")){
+	    	   
+	    	   if(cor.contains("rgb")){
+	        		String  str = cor.replaceAll("[^-?0-9]+", " "); 
+	        		
+	        		backgroundColor = new Color(
+	        				Integer.parseInt(str.trim().split(" ")[0]),
+	        				Integer.parseInt(str.trim().split(" ")[1]),
+	        				Integer.parseInt(str.trim().split(" ")[2]));
+	    	   }else  backgroundColor = new Color(Integer.parseInt(cor.trim().replaceFirst("^#",""), 16));
+	    	   
+	       }else isProces = false;
+	           
+	    }    
+	    
+	   //background
+	    Pattern backgroundColorValue = Pattern.compile("background-color:(.*?)\\;");
+		match = backgroundColorValue.matcher(style);
+	    
+		if(match.find()){
+			String cor = match.group(1);
+			
+	        //System.out.println(cor);
+	        if(!isTextCor(cor)){
+	        	
+	        	if(cor.contains("rgb") && !cor.contains("url")){
+	        		String  str = cor.replaceAll("[^-?0-9]+", " "); 
+	        		
+	        		backgroundColor = new Color(
+	        				Integer.parseInt(str.trim().split(" ")[0]),
+	        				Integer.parseInt(str.trim().split(" ")[1]),
+	        				Integer.parseInt(str.trim().split(" ")[2]));
+	        	}else backgroundColor = new Color(Integer.parseInt(cor.trim().replaceFirst("^#",""), 16));
+	        
+	        }else isProces = false;
+	    }
+	    
+		if(isProces){
+			avaliadorContraste.setCores(foregroundColor,backgroundColor);
+			avaliadorContraste.avaliar();
+			
+			if(avaliadorContraste.getContraste() < new Double("4.5")){
+				System.out.println("foreground: "+foregroundColor.getRGB()+" -- "+"background"+backgroundColor.getRGB()+" Resultado: "+avaliadorContraste.getContraste());
+				return true;
+			}
+			
+		}
+		
+		return false;
 	}
 	
 	private List<Occurrence> checkRecommendation30() {
@@ -186,7 +398,7 @@ public class PresentationEvaluation extends Evaluation{
 					
 					if(!href.startsWith("http")) href = getUrl()+href;
 					
-					String content = WebAgent.from(href).withGetRequest().execute().getContent();
+					 String content = WebAgent.from(href.replace(" ", "%20")).withGetRequest().execute().getContent();
 					
 					 if (content!=null && !content.contains("a:hover") && !content.contains("a:focus")){
 						 occurrences.add(buildOccurrence("4.4", true, link.toString(), link, "1"));
