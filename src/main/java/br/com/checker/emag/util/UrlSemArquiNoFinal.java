09aabc8c64b2609209e3df4c4f3ab39dee77d765
gibran <gibransodre@gmail.com>
2016-06-15 08:56:17 -0300

    Método para deixar a url sem arquivo no final.

---
+++ src/main/java/br/com/checker/emag/util/UrlSemArquiNoFinal.java
@@ -0,0 +1,40 @@
+package br.com.checker.emag.util;
+
+public class UrlSemArquiNoFinal {
+	
+	public String urlSemArquivoNoFinal(String url)
+	{
+		if(url != null)
+		{
+		String urlSemArquivo = "";
+		boolean encontrouPonto = false;
+		
+	
+		for(int i = url.length()-1; i >= 0; i-- )
+		{
+						
+			urlSemArquivo = url.substring(i-1,i);
+			
+			if(encontrouPonto && urlSemArquivo.equalsIgnoreCase("."))
+			{
+				break;
+				
+			}else if(urlSemArquivo.equalsIgnoreCase("."))
+			{					
+				encontrouPonto = true;					
+				
+			}else  if(encontrouPonto && urlSemArquivo.equalsIgnoreCase("/"))
+			{
+				url = url.substring(0,i);
+				break;
+			}
+			else if(!encontrouPonto && urlSemArquivo.equalsIgnoreCase("/"))					
+			{
+				break;
+			}
+		}
+		}
+		return url;
+	}
+
+}
